358bc8fe1c3b816d9a9bd86fcaf7385934fbf74b
DanielaAlvarezEPFL <alvarez.danielapamelin@epfl.ch>
2024-11-15 19:54:15 +0100

    general.py

--- general.py
+++ general.py
@@ -8,13 +8,9 @@ import numpy as np
 file_path = './data/movies_dataset.tsv'
 movies_df = pd.read_csv(file_path, sep='\t')
 
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 def plot_top_10_countries(movies_df):
- 
-    # Count different countries
-    country_counts = movies_df['Movie countries'].explode().value_counts()  # Unpacks multiple countries per row
+    country_counts = movies_df['Movie countries'].str.split(',').explode().str.strip().value_counts()
     top_10_countries = country_counts.head(10)
 
     # Plotting
@@ -24,4 +20,6 @@ def plot_top_10_countries(movies_df):
     plt.xlabel('Country')
     plt.ylabel('Number of Movies')
     plt.xticks(rotation=45)
+    plt.tight_layout()  
     plt.show()
+
